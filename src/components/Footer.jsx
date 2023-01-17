34f0d86647bf774b7123d318accc770de590968a
Yooni <yooni.kj.yoon@gmail.com>
2023-01-17 17:34:09 +0900

    add placeholder text in input box

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <div className="footer-wrap">
       <div className="input-wrap">
-        <input className="todo-input" />
+        <input className="todo-input" placeholder="Add todo here" />
         <div className="button-add">Add</div>
       </div>
     </div>
